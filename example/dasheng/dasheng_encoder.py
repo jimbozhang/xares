5c81f3d5a9f5f1306728a6925291a314bad59f3a
liuchenyu3 <liuchenyu3@xiaomi.com>
2024-07-09 20:09:48 +0800

    fix dasheng pretrained problem

--- example/dasheng/dasheng_encoder.py
+++ example/dasheng/dasheng_encoder.py
@@ -1,12 +1,35 @@
+import torch
 from dataclasses import dataclass
-
-from dasheng import dasheng_base
-
 from xares.audio_encoder_base import AudioEncoderBase
+from dasheng.train.models import AudioTransformerMAE_Encoder
+from dasheng.pretrained.pretrained import *
+
+class Dasheng(Dasheng):
+    @classmethod
+    def from_pretrained(
+            cls, pretrained_url: str,
+            **additional_model_kwargs) -> AudioTransformerMAE_Encoder:
+        """
+        Class method to create a new Vocos model instance from a pre-trained model stored in the Hugging Face model hub.
+        """
+        if 'http' in pretrained_url:
+            dump = torch.hub.load_state_dict_from_url(pretrained_url,
+                                                      map_location='cpu')
+        else:
+            dump = torch.load(pretrained_url, map_location='cpu')
+    
+        model_parmeters, model_config = dump['model'], dump['config']
+        model_config.update({
+            'depth':12, 'num_heads':12, 'embed_dim':768,
+        })
+        instance = cls(**{**model_config, **additional_model_kwargs})
+        instance.load_state_dict(model_parmeters, strict=True)
+        return instance
+
 
 @dataclass
 class DashengEncoder(AudioEncoderBase):
-    model = dasheng_base()
+    model = Dasheng.from_pretrained(PRETRAINED_CHECKPOINTS['dasheng_base'])
     sampling_rate = 16_000 # model sr
     output_dim = 768
 
