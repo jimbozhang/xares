42a09de273334eb387c0adbce7c83926add67361
Junbo Zhang <zhangjunbo1@xiaomi.com>
2025-01-21 23:22:52 +0800

    Fix wav2vec2 encoder

--- example/wav2vec2/wav2vec2_encoder.py
+++ example/wav2vec2/wav2vec2_encoder.py
@@ -4,6 +4,7 @@ from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model
 
 class Wav2vec2Encoder(nn.Module):
     def __init__(self):
+        super().__init__()
         self.sampling_rate = 16000
         self.model_name = "facebook/wav2vec2-large-100k-voxpopuli"
         self.cache_dir = "pretrained/"
@@ -16,9 +17,4 @@ class Wav2vec2Encoder(nn.Module):
             audio, sampling_rate=self.sampling_rate, return_tensors="pt"
         ).input_values.squeeze()
 
-        encoded_audio = self.encode_audio(input_values)["last_hidden_state"]
-
-        if not self.check_encoded_audio(encoded_audio):
-            raise ValueError("Invalid encoded audio")
-
-        return encoded_audio
+        return self.model(input_values)["last_hidden_state"]
