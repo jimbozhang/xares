baf7f2d4d4fd2322dd8c417c1560c09db23d7cd9
Junbo Zhang <zhangjunbo1@xiaomi.com>
2025-02-03 18:44:59 +0800

    Add whisper example

--- example/wav2vec2/wav2vec2_encoder.py
+++ example/wav2vec2/wav2vec2_encoder.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import torch.nn as nn
 from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model
 
@@ -9,8 +11,21 @@ class Wav2vec2Encoder(nn.Module):
         self.model_name = "facebook/wav2vec2-large-100k-voxpopuli"
         self.output_dim = 1024
         self.hop_size_in_ms = 20
+
+        if not Path(self.model_name).exists():
+            self.download_from_hub(self.model_name)
+
         self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(self.model_name)
         self.model = Wav2Vec2Model.from_pretrained(self.model_name)
+
+    @classmethod
+    def download_from_hub(self, model_name: str, output_root: str = "."):
+        # Saving to local to avoid multiprocessing issues
+        output_dir = Path(output_root) / model_name
+        self.processor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
+        self.model = Wav2Vec2Model.from_pretrained(model_name)
+        self.processor.save_pretrained(output_dir)
+        self.model.save_pretrained(output_dir)
 
     def forward(self, audio):
         input_values = self.feature_extractor(
---
+++ example/whisper/whisper_encoder.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import torch
+from transformers import WhisperModel, WhisperProcessor
+
+
+class WhisperEncoder(torch.nn.Module):
+    def __init__(self, model_name="openai/whisper-base"):
+        super().__init__()
+
+        if not Path(model_name).exists():
+            self.download_from_hub(model_name)
+
+        self.processor = WhisperProcessor.from_pretrained(model_name)
+        self.model = WhisperModel.from_pretrained(model_name).get_encoder()
+
+        self.sampling_rate = self.processor.feature_extractor.sampling_rate
+        self.output_dim = self.model.config.d_model
+        self.hop_size_in_ms = self.processor.feature_extractor.hop_length
+
+    @classmethod
+    def download_from_hub(self, model_name: str, output_root: str = "."):
+        # Saving to local to avoid multiprocessing issues
+        output_dir = Path(output_root) / model_name
+        self.processor = WhisperProcessor.from_pretrained(model_name)
+        self.model = WhisperModel.from_pretrained(model_name)
+        self.processor.save_pretrained(output_dir)
+        self.model.save_pretrained(output_dir)
+
+    def forward(self, audio: torch.Tensor):
+        assert isinstance(audio, torch.Tensor)
+        if audio.ndim == 1:
+            audio = audio.unsqueeze(0)
+
+        audio = audio.cpu().numpy()
+
+        # Feature extraction
+        features = self.processor(audio, sampling_rate=self.sampling_rate, return_tensors="pt")
+
+        # Model forward pass
+        features["input_features"] = features["input_features"].to(self.model.device)
+        return self.model(**features).last_hidden_state
+
+
+if __name__ == "__main__":
+    from xares.audio_encoder_checker import check_audio_encoder
+
+    encoder = WhisperEncoder()
+    check_audio_encoder(encoder)
