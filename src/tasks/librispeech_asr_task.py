4f47220db70879f9f8598ba22393626a9ade0838
Junbo Zhang <zhangjunbo1@xiaomi.com>
2025-04-14 11:39:06 +0800

    Disable librispeech_asr and maestro by default

--- src/tasks/librispeech_asr_task.py
+++ src/tasks/librispeech_asr_task.py
@@ -8,7 +8,7 @@ def librispeech_asr_config(encoder) -> TaskConfig:
         encoder=encoder,
         eval_weight=10000,
         batch_size_train=16,
-        disabled=False,
+        disabled=True,
         do_knn=False,
         formal_name="LibriSpeech-100h",
         gradient_accumulation_steps=4,
